ddf7f0bed32a43c7d94f980a917683daac9b1580
Michael <michael.s.baumberger@gmail.com>
2013-10-17 22:08:48 +1300

    Finish chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,12 +10,17 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
-  
+
   describe "Help page" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
@@ -27,7 +32,7 @@ describe "Static pages" do
       expect(page).to have_title("#{base_title} | Help")
     end
   end
-  
+
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
